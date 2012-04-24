88baefe66879ac63edfe8b229d682c53c5a0c702
Sergey Kozlov <xxegax@gmail.com>
2012-04-24 13:30:52 +0400

    fix appearance loading

--- appearance.js
+++ appearance.js
@@ -1,20 +1,44 @@
+var prevVals=[];
 function saveValue(key, value)
 {
 	if((typeof value) != "string")
 		value = JSON.stringify(value);
+	if(prevVals[key]==value)
+		return;
+		
+	prevVals[key]=value;
+	
 	$.ajax({url: "appr.php?key="+key+"&act=set", type:"POST", data: value}).done(function(msg){});
 }
 
+function removeNulls(arr)
+{
+	for(var k in arr)
+	{
+		v=arr[k];
+		if(typeof v=="object" && v===null)
+			delete arr[k];
+		else if(v instanceof Array)
+			removeNulls(v);
+		else if(v instanceof Object)
+			removeNulls(v);
+	}
+}
+	
 function loadValue(key, callback)
 {
 	$.ajax({url:"appr.php?key="+key+"&act=get"}).done(function(msg)
 	{
-		callback(JSON.parse(msg));
+		var arr=JSON.parse(msg);
+		removeNulls(arr);
+		callback(arr);
 	}).error(function()
 	{
 		$.ajax({url:"animData.js"}).done(function(msg)
 		{
-			callback(JSON.parse(msg));
+			var arr=JSON.parse(msg);
+			removeNulls(arr);
+			callback(arr);
 		});
 	});
 }
